da7fefa1b5b202baadf7761f98fcd14e6e6b24ed
Daniel aka Iferus <mamsmotri@gmail.com>
2018-02-12 03:23:20 +0700

    123

---
+++ dependencies.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import config
+import telebot
+from telebot import types
+import requests
+from bs4 import BeautifulSoup
+import re
+import json
+from pprint import pprints
